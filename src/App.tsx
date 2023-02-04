edf7fe62d14ca9bb26df9df66121c5d0573954d2
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-05 00:22:34 +1100

    Navigation Bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
 import Home from './Routes/Home/Home.component';
 import React from 'react';
+
+const Navigation = () => {
+	return (
+		<div>
+			<div>
+				<h1>I am the navigation bar</h1>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
 
 const Shop = () => {
 	return <h1>I am the shop page</h1>;
@@ -10,7 +21,8 @@ const Shop = () => {
 export default function App() {
 	return (
 		<Routes>
-			<Route path='/home' element={<Home />}>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
 				<Route path='shop' element={<Shop />} />
 			</Route>
 		</Routes>
