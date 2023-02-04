3d2c3cb571fa7cc3905959e902bea8671ad030f9
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-05 00:43:08 +1100

    Styling for Navigation + Logo

--- src/Routes/Navigation/Navigation.component.tsx
+++ src/Routes/Navigation/Navigation.component.tsx
@@ -1,18 +1,20 @@
+import './navigation.styles.scss';
+
 import { Link, Outlet } from 'react-router-dom';
 
+import { ReactComponent as CrwnLogo } from './../../assets/crown.svg';
 import { Fragment } from 'react';
-import Home from 'Routes/Home/Home.component';
 
 export default function Navigation() {
 	return (
 		<Fragment>
 			<div className='navigation'>
 				<Link className='logo-container' to='/'>
-					<div>Logo</div>
+					<CrwnLogo className='logo' />
 				</Link>
 				<div className='nav-links-container'>
 					<Link className='nav-link' to='/shop'>
-						Shop
+						SHOP
 					</Link>
 				</div>
 			</div>
