e7b53c578b811ff32b154a912956381b08cc0ea6
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-04 22:06:31 +1100

    Setting Up Our Categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,40 @@
+import { Category } from './Models/category';
 import React from 'react';
 
 const App = () => {
+	const categories: Category[] = [
+		{
+			id: 1,
+			title: 'Hats',
+		},
+		{
+			id: 2,
+			title: 'Jackets',
+		},
+		{
+			id: 3,
+			title: 'Sneakers',
+		},
+		{
+			id: 4,
+			title: 'Women',
+		},
+		{
+			id: 5,
+			title: 'Mens',
+		},
+	];
 	return (
 		<div className='categories-container'>
-			<div className='category-container'>
-				{/* <img /> */}
-				<div className='category-body-container'>
-					<h2>Hats</h2>
-					<p>Shop Now</p>
+			{categories.map((category) => (
+				<div key={category.id} className='category-container'>
+					<div className='background-image' />
+					<div className='category-body-container'>
+						<h2>{category.title}</h2>
+						<p>Shop Now</p>
+					</div>
 				</div>
-			</div>
-			<div className='category-container'>
-				{/* <img /> */}
-				<div className='category-body-container'>
-					<h2>Jackets</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className='category-container'>
-				{/* <img /> */}
-				<div className='category-body-container'>
-					<h2>Sneakers</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className='category-container'>
-				{/* <img /> */}
-				<div className='category-body-container'>
-					<h2>Women</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className='category-container'>
-				{/* <img /> */}
-				<div className='category-body-container'>
-					<h2>Mens</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
