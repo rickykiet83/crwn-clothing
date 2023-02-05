f16f2b7fd566dd2f8ddd0087ef2ea85b2172b3d3
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-05 21:08:00 +1100

    Setting Up Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Routes/Home/Home.component';
 import Navigation from 'Routes/Navigation/Navigation.component';
 import React from 'react';
+import SignIn from 'Routes/SigIn/SignIn.component';
 
 const Shop = () => {
 	return <h1>I am the shop page</h1>;
@@ -14,6 +15,7 @@ export default function App() {
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path='shop' element={<Shop />} />
+				<Route path='sign-in' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
