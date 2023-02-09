1f0f353ee3b95e6bc6f93037c6349d21e4c26779
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-09 12:40:22 +1100

    Introducing Styled-components

--- src/Routes/Navigation/Navigation.component.tsx
+++ src/Routes/Navigation/Navigation.component.tsx
@@ -1,6 +1,10 @@
-import './navigation.styles.scss';
-
 import { Link, Outlet } from 'react-router-dom';
+import {
+	LogoContainer,
+	NavLink,
+	NavLinks,
+	NavigationContainer,
+} from './navigation.styles';
 
 import { CartContext } from 'contexts/cart.context';
 import CartDropdown from './../../components/cart-dropdown/cart-dropdown.component';
@@ -17,27 +21,23 @@ export default function Navigation() {
 
 	return (
 		<Fragment>
-			<div className='navigation'>
-				<Link className='logo-container' to='/'>
+			<NavigationContainer>
+				<LogoContainer to='/'>
 					<CrwnLogo className='logo' />
-				</Link>
-				<div className='nav-links-container'>
-					<Link className='nav-link' to='/shop'>
-						SHOP
-					</Link>
+				</LogoContainer>
+				<NavLinks>
+					<NavLink to='/shop'>SHOP</NavLink>
 					{currentUser ? (
-						<span className='nav-link' onClick={signOutUser}>
+						<NavLink as='span' onClick={signOutUser}>
 							SIGN OUT
-						</span>
+						</NavLink>
 					) : (
-						<Link className='nav-link' to='/auth'>
-							SIGN IN
-						</Link>
+						<NavLink to='/auth'>SIGN IN</NavLink>
 					)}
 					<CartIcon />
-				</div>
+				</NavLinks>
 				{isCartOpen && <CartDropdown />}
-			</div>
+			</NavigationContainer>
 			<Outlet />
 		</Fragment>
 	);
