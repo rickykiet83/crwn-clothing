5615d707b31571f2ee60c147c984338068981afb
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-09 13:03:58 +1100

    Styled-Components-Button

--- src/Components/Button/Button.component.tsx
+++ src/Components/Button/Button.component.tsx
@@ -1,20 +1,30 @@
 import './button.styles.scss';
 
+import {
+	BaseButton,
+	GoogleSignInButton,
+	InvertedButton,
+} from './button.styles';
 import React, { ButtonHTMLAttributes } from 'react';
 
 import { BUTTON_TYPE_CLASSES } from './../../models/button-type.enum';
 
+const getButton = (buttonType: BUTTON_TYPE_CLASSES) =>
+	({
+		[BUTTON_TYPE_CLASSES.base]: BaseButton,
+		[BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+		[BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+	}[buttonType || BUTTON_TYPE_CLASSES.base]);
+
 const Button: React.FC<
 	{
-		buttonType?: string;
+		buttonType?: BUTTON_TYPE_CLASSES;
 	} & ButtonHTMLAttributes<HTMLButtonElement>
 > = ({ children, buttonType, ...otherProps }) => {
 	const type = buttonType ?? BUTTON_TYPE_CLASSES.base;
-	return (
-		<button className={`button-container ${type}`} {...otherProps}>
-			{children}
-		</button>
-	);
+	const CustomButton = getButton(type);
+
+	return <CustomButton {...otherProps}>{children}</CustomButton>;
 };
 
 export default Button;
