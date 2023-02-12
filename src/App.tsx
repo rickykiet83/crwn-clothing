0c24166f763501f4023065f6c6af0640098f5ac2
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-12 16:55:40 +1100

    Lazy Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Authentication from '@routes/authentication/authentication.component';
-import Checkout from '@routes/checkout/checkout.component';
-import Home from '@routes/home/home.component';
-import Navigation from '@routes/navigation/navigation.component';
-import Shop from '@routes/shop/shop.component';
 import { checkUserSession } from '@store/user/user.action';
 import { useDispatch } from 'react-redux';
+
+const Navigation = lazy(
+	() => import('@routes/navigation/navigation.component')
+);
+
+const Home = lazy(() => import('@routes/home/home.component'));
+const Shop = lazy(() => import('@routes/shop/shop.component'));
+const Authentication = lazy(
+	() => import('@routes/authentication/authentication.component')
+);
+const Checkout = lazy(() => import('@routes/checkout/checkout.component'));
 
 export default function App() {
 	const dispatch = useDispatch();
@@ -16,13 +22,15 @@ export default function App() {
 	}, []);
 
 	return (
-		<Routes>
-			<Route path='/' element={<Navigation />}>
-				<Route index element={<Home />} />
-				<Route path='shop/*' element={<Shop />} />
-				<Route path='auth' element={<Authentication />} />
-				<Route path='checkout' element={<Checkout />} />
-			</Route>
-		</Routes>
+		<Suspense>
+			<Routes>
+				<Route path='/' element={<Navigation />}>
+					<Route index element={<Home />} />
+					<Route path='shop/*' element={<Shop />} />
+					<Route path='auth' element={<Authentication />} />
+					<Route path='checkout' element={<Checkout />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 }
